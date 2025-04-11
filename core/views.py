ac2a9f18fa3363e4e5a95bbb63edbbe54214b3d9
Garett.Janulewicz <janulewiczgarett@gmail.com>
2025-04-11 13:40:36 -1000

    home

--- core/views.py
+++ core/views.py
@@ -14,8 +14,8 @@ import csv # debug
 import time #debug
 
 
-def landing(request):
-    return render(request, 'core/landing.html')
+def home(request):
+    return render(request, 'core/home.html')
 
 def index(request):
 
