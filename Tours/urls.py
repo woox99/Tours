aa7611790bddc1be861d065f5125b8688f424555
Garett.Janulewicz <janulewiczgarett@gmail.com>
2025-07-29 18:36:28 -1000

    sitemap.xml and favicon

--- Tours/urls.py
+++ Tours/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from core.sitemaps import StaticViewSitemap, IslandSitemap, CategoryByIslandSitemap
+from django.views.generic import RedirectView
+from django.conf import settings
 
 sitemaps = {
     'static': StaticViewSitemap,
@@ -13,6 +15,7 @@ handler404 = 'core.views.error_404_view'
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('sitemap.xml/', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'core/favicon.ico', permanent=True)),
     path('', include('core.urls')),
 ]
